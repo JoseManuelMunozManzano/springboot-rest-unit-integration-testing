876c29a1028db41a980bded38aba1c081355bf75
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-02 09:23:41 +0200

    Test DELETE. Borrar un grade válido

--- src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -2,6 +2,7 @@ package com.luv2code.springmvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.MathGrade;
 import com.luv2code.springmvc.repository.HistoryGradesDao;
 import com.luv2code.springmvc.repository.MathGradesDao;
 import com.luv2code.springmvc.repository.ScienceGradesDao;
@@ -269,6 +270,25 @@ public class GradebookControllerTest {
                 .andExpect(status().is4xxClientError())
                 .andExpect(jsonPath("$.status", is(404)))
                 .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
+    }
+
+    @Test
+    void deleteAValidGradeHttpRequest() throws Exception {
+        // Verificamos que el grade existe antes de borrarlo
+        Optional<MathGrade> mathGrade = mathGradeDao.findById(1);
+        assertTrue(mathGrade.isPresent());
+
+        // Hacemos el delete
+        // Esperamos un status 200
+        // Y un contenido JSON con los datos del estudiante y ningún grade, ya que se ha borrado
+        mockMvc.perform(MockMvcRequestBuilders.delete("/grades/{id}/{gradeType}", 1, "math"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstname", is("Eric")))
+                .andExpect(jsonPath("$.lastname", is("Roby")))
+                .andExpect(jsonPath("$.emailAddress", is("eric.roby@luv2code_school.com")))
+                .andExpect(jsonPath("$.studentGrades.mathGradeResults", hasSize(0)));
     }
 
     @AfterEach
